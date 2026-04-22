a54eb9f51d117072bc3f5cdbd4ef791c70e52267
Trey Bailey <tdbailey05@gmail.com>
2026-04-22 09:23:09 -0600

    Delete js directory

--- js/main.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    initValidation("myform", "success");
-});
-
-document.getElementById("formBtn").addEventListener("click", function () {
-    document.getElementById("form").scrollIntoView({ behavior: "smooth" });
-});
